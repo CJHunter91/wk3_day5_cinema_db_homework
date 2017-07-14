222d63dd593622f3fa04aad03452cf9b24767e42
Chris Hunter <orkem2@gmail.com>
2017-07-14 13:58:49 +0100

    Added update to customer class working

--- db/console.rb
+++ db/console.rb
@@ -4,15 +4,20 @@ require_relative('../models/film')
 require('pry')
 
 customer_chris = Customer.new({"name" => "Chris", "funds" => 100})
-
 customer_chris.save
 
-raiders = Film.new({"title" => "Raiders of the lost ark", "price" => 13})
+customer2 = Customer.new({"name" => "Rick", "funds" => 50})
+customer2.save
 
+
+
+raiders = Film.new({"title" => "Raiders of the lost ark", "price" => 13})
 raiders.save
 
 c_ticket_raiders = Ticket.new({"customer_id" =>customer_chris.id, "film_id" => raiders.id})
-
 c_ticket_raiders.save
+
+customer2.name = "John"
+customer2.update
 binding.pry
 nil
--- models/customer.rb
+++ models/customer.rb
@@ -2,6 +2,7 @@ require_relative('../db/sql_runner')
 class Customer
 
   attr_reader :id
+  attr_accessor :name, :funds
 
   def initialize(options)
     @id = options['id'].to_i if options['id']
@@ -17,6 +18,14 @@ class Customer
      ('#{@name}', #{@funds}) 
      RETURNING id;"
      @id = SqlRunner.run(sql)[0]['id'].to_i
+  end
+
+  def update
+    sql = "UPDATE customers SET
+    (name, funds) =
+    ('#{@name}', #{@funds})
+    WHERE id = #{@id}"
+    SqlRunner.run(sql)
   end
 
 
