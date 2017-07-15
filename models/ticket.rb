2bd163b1f1da216472e2d9f7750d8168bed967b5
Chris Hunter <orkem2@gmail.com>
2017-07-15 10:06:16 +0100

    Added screening function to ticket. Ticket function now calls sreening to return a film id

--- models/ticket.rb
+++ models/ticket.rb
@@ -1,5 +1,6 @@
 require_relative('../db/sql_runner')
 require_relative('screening')
+require('pry')
 class Ticket
 
   attr_reader :id
@@ -8,17 +9,20 @@ class Ticket
   def initialize(options)
     @id = options['id'].to_i if options['id']
     @customer_id = options['customer_id']
-    @film_id = options['film_id'] 
+    @film_id = options['film_id'] if options['film_id'] 
     @screening_id = options['screening_id']
   end
 
   def save
+    screening()
+    #create the screening object to return the asscociated film id
     sql = "INSERT INTO tickets
      (customer_id, film_id, screening_id) 
      VALUES
      (#{@customer_id}, #{@film_id}, #{@screening_id}) 
      RETURNING id;"
      @id = SqlRunner.run(sql)[0]['id'].to_i
+     
   end
 
   def update
@@ -40,6 +44,14 @@ class Ticket
 
   def self.delete_all
     SqlRunner.run("DELETE FROM tickets")
+  end
+
+  def screening
+    #function returns the correct film id for the ticket using the screening object
+    #This stops the need for giving ticket an extra parameter on init
+    sql = "SELECT film_id FROM screenings
+    WHERE id = #{@screening_id}"
+    @film_id = Screening.map_screenings(sql)[0].film_id.to_i
   end
 
   def self.map_tickets(sql)
